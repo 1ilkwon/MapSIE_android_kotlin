6a2263cadb322668e492d35c2b20e4728b5e22da
1ilkwon <dlfrnjs318@gmail.com>
2022-05-25 17:12:32 +0900

    merge 진행중 1
    진행중 1

---
+++ MapSIE/app/src/main/java/kr/ac/tukorea/mapsie/SplashActivity.kt
@@ -0,0 +1,26 @@
+package kr.ac.tukorea.mapsie
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import kr.ac.tukorea.mapsie.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+        startLoading()
+    }
+
+    private fun startLoading() {
+
+        Handler().postDelayed({
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1250)
+    }
+}
