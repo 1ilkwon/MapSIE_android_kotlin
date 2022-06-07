7d1476612f8c0941c48e36d65bf3ee919644f038
1ilkwon <dlfrnjs318@gmail.com>
2022-06-07 23:16:46 +0900

    검색기능 구현중

--- MapSIE/app/src/main/java/kr/ac/tukorea/mapsie/MapPage/ThemePlaceAdapter.kt
+++ MapSIE/app/src/main/java/kr/ac/tukorea/mapsie/MapPage/ThemePlaceAdapter.kt
@@ -6,16 +6,24 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.theme_item.view.*
 import kotlinx.android.synthetic.main.theme_place_item.view.*
 import kr.ac.tukorea.mapsie.DetailActivity
 import kr.ac.tukorea.mapsie.MapActivity
 import kr.ac.tukorea.mapsie.R
+import kr.ac.tukorea.mapsie.ThemeData
 
 
 class ThemePlaceAdapter(private val context: Context, private val themePlaceList: ArrayList<ThemePlaceList>):
-    RecyclerView.Adapter<ThemePlaceAdapter.ViewHolder>() {
+    RecyclerView.Adapter<ThemePlaceAdapter.ViewHolder>(), Filterable {
+    //필터 전 리스트
+    var unfilter = themePlaceList
+    //필터를 위한 변수
+    var filter = themePlaceList
+    val item = filter[position]
 
     override fun getItemCount(): Int = themePlaceList.size
 
@@ -56,6 +64,35 @@ class ThemePlaceAdapter(private val context: Context, private val themePlaceList
                 LayoutInflater.from(context).inflate(R.layout.theme_place_item, parent, false)
             return ViewHolder(view)
         }
+
+    //리사이클뷰 필터링 메서드
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if (charSearch.isEmpty()) {
+                    filter = unfilter
+                } else {
+                    var resultList = ArrayList<ThemePlaceList>()
+                    for (item in unfilter) {
+                        if (item.title.toLowerCase().contains(charSearch.toLowerCase()))
+                            resultList.add(item)
+                    }
+                    filter = resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = filter
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                filter = results?.values as ArrayList<ThemePlaceList>
+                notifyDataSetChanged()
+            }
+
+        }
+    }
 
     }
 
