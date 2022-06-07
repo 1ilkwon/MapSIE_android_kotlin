62e09d8599d62c38eec62065eba74f6705a85857
HuiKyeongPark <qkrgmlrud@tukorea.ac.kr>
2022-06-08 02:13:53 +0900

    프로필 사진 업로드
    프로필 사진 업로드

--- MapSIE/app/src/main/java/kr/ac/tukorea/mapsie/MainActivity.kt
+++ MapSIE/app/src/main/java/kr/ac/tukorea/mapsie/MainActivity.kt
@@ -19,16 +19,15 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_detail.*
-import kotlinx.android.synthetic.main.main_body.*
 import kotlinx.android.synthetic.main.main_drawer_header.*
 import kotlinx.android.synthetic.main.main_toolbar.*
-import kotlinx.android.synthetic.main.theme_detail_list_n_search.view.*
 import kr.ac.tukorea.mapsie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     // firestore 연결 위해 기입
     var db: FirebaseFirestore = Firebase.firestore
+
     var pos = 0
     var i : Int = 1
     var dataArr = arrayOf(
@@ -83,10 +82,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         db.collection("users").document(Firebase.auth.currentUser?.uid ?: "No User").get().addOnSuccessListener {
             member_nickname.text = it["signName"].toString()
-            Glide.with(this).load(it["signImg"]).into(member_icon)
+            Glide.with(this)
+                .load(it["signImg"])
+                .override(60, 60)
+                .error(R.drawable.ic_baseline_account_circle_24)    //에러가 났을 때
+                .fallback(R.drawable.ic_baseline_account_circle_24) //signImg값이 없다면 기본 사진 출력
+                .into(member_icon)
         }.addOnFailureListener {
             Toast.makeText(this, ".", Toast.LENGTH_SHORT).show()
         }
+
 
         initRecycler()  //recyclerview 보여주는 메서드
     }
